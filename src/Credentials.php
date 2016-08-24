6981efbb2b0ffd5514e4d18f4c8cffa6763897c0
LUMASERV <info@lumaserv.com>
2016-08-25 01:08:12 +0200

    Update Credentials.php

--- src/Credentials.php
+++ src/Credentials.php
@@ -11,6 +11,10 @@ class Credentials
 
     public function __construct($token, $debug)
     {
+        if (!is_string($token)) {
+            throw new MalformedParameterException('invalid argument');
+        }
+        
         $this->token = $token;
 
         switch ($debug) {

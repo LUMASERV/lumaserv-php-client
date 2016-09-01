4f7100d540462f08d7f31a15f2e0789c3f56d0d0
LUMASERV <info@lumaserv.com>
2016-09-01 23:46:03 +0000

    Applied fixes from StyleCI

--- src/Domain.php
+++ src/Domain.php
@@ -3,16 +3,13 @@
  * Created by PhpStorm.
  * User: Jan Waldecker
  * Date: 02.09.2016
- * Time: 00:33
+ * Time: 00:33.
  */
-
 namespace LumaservSystems;
-
 
 class Domain
 {
     public function __construct()
     {
-
     }
 }
--- src/LUMASERV.php
+++ src/LUMASERV.php
@@ -61,85 +61,99 @@ class LUMASERV
     }
 
     /**
-     * @param string $actionPath    The resource path you want to request, see more at the documentation.
-     * @param array $params         Array filled with request params
-     * @param string $method        HTTP method used in the request
+     * @param string $actionPath The resource path you want to request, see more at the documentation.
+     * @param array  $params     Array filled with request params
+     * @param string $method     HTTP method used in the request
+     *
+     * @throws MalformedParameterException If the given field in params is not an array
      *
      * @return ResponseInterface
-     *
-     * @throws MalformedParameterException  If the given field in params is not an array
      */
-    private function request($actionPath, $params = [], $method = 'GET') {
+    private function request($actionPath, $params = [], $method = 'GET')
+    {
         $url = $this->getCredentials()->getUrl().$actionPath;
 
-        if (!is_array($params))
+        if (!is_array($params)) {
             throw new MalformedParameterException();
+        }
 
         $params['token'] = $this->getCredentials()->getToken();
 
         switch ($method) {
             case 'GET':
                 return $this->getHttpClient()->get($url, [
-                    'query' => $params
+                    'query' => $params,
                 ]);
                 break;
             case 'POST':
                 return $this->getHttpClient()->post($url, [
-                    'body' => $params
+                    'body' => $params,
                 ]);
                 break;
             case 'PUT':
                 return $this->getHttpClient()->put($url, [
-                    'body' => $params
+                    'body' => $params,
                 ]);
             case 'DELETE':
                 return $this->getHttpClient()->delete($url, [
-                    'body' => $params
+                    'body' => $params,
                 ]);
             default:
-                throw new MalformedParameterException("Wrong HTTP method passed");
+                throw new MalformedParameterException('Wrong HTTP method passed');
         }
     }
 
     /**
-     *
      * @param $response ResponseInterface
+     *
      * @return \Psr\Http\Message\StreamInterface
      */
-    private function processRequest($response) {
+    private function processRequest($response)
+    {
         $response = $response->getBody();
         $result = json_decode($response);
-        if (json_last_error() == JSON_ERROR_NONE)
+        if (json_last_error() == JSON_ERROR_NONE) {
             return $response;
-        else
+        } else {
             return $result;
+        }
     }
 
-    public function get($actionPath, $params = []) {
+    public function get($actionPath, $params = [])
+    {
         $response = $this->request($actionPath, $params);
+
         return $this->processRequest($response);
     }
 
-    public function put($actionPath, $params = []) {
+    public function put($actionPath, $params = [])
+    {
         $response = $this->request($actionPath, $params, 'PUT');
+
         return $this->processRequest($response);
     }
 
-    public function post($actionPath, $params = []) {
+    public function post($actionPath, $params = [])
+    {
         $response = $this->request($actionPath, $params, 'POST');
+
         return $this->processRequest($response);
     }
 
-    public function delete($actionPath, $params = []) {
+    public function delete($actionPath, $params = [])
+    {
         $response = $this->request($actionPath, $params, 'DELETE');
+
         return $this->processRequest($response);
     }
 
-    public function getUrl($actionPath = '') {
+    public function getUrl($actionPath = '')
+    {
         return $this->getCredentials()->getUrl().$actionPath;
     }
 
-    public function getAllDomains() {
+    public function getAllDomains()
+    {
         return $this->get('domains');
     }
 }
